2cf5d2bacd74fa4e27fa1e9fa0b07ef1a6a3867f
J. Damaricio Gomes <damaricio@gmail.com>
2015-01-28 20:25:45 -0500

    My scripts from the other game !

---
+++ 8Zilla/Assets/Scripts/SmashBar.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmashBar : MonoBehaviour
+{
+
+		public float speed = 5f;
+		public float smeshPosition = 5f;
+		bool inverted = false;
+		public bool opositeDirection = false;
+		float countTime = 0f;
+		float initialYPosition;
+		// Use this for initialization
+		void Start ()
+		{
+				initialYPosition = transform.position.y;
+		}
+
+		// Update is called once per frame
+		void Update ()
+		{
+	
+
+				Vector2 currentPosition = transform.position;
+
+			
+				if (!opositeDirection) { 
+						if (!inverted && currentPosition.y < (initialYPosition + smeshPosition)) {
+								currentPosition.y += Time.deltaTime * speed;
+						} else {
+								inverted = true;
+						}
+						
+						if (inverted && currentPosition.y >= initialYPosition) {
+								currentPosition.y -= Time.deltaTime * speed;
+						} else {
+								inverted = false;		
+						}
+				} else {
+						if (!inverted && currentPosition.y > (initialYPosition - smeshPosition)) {
+								currentPosition.y -= Time.deltaTime * speed;
+						} else {
+								inverted = true;
+						}
+			
+						if (inverted && currentPosition.y <= initialYPosition) {
+								currentPosition.y += Time.deltaTime * speed;
+						} else {
+								inverted = false;		
+						}
+				}
+				transform.position = currentPosition;
+		}
+}
