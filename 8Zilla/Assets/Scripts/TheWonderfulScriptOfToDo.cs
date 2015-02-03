8c3601698844da7da8f0423b74270aa9c9ebb19a
unknown <1411595@5B1-22.ad.dawsoncollege.qc.ca>
2015-02-02 19:53:56 -0500

    added an enemy folder and rambo is prefabed also I wrote a todo script and that's it i think

---
+++ 8Zilla/Assets/Scripts/TheWonderfulScriptOfToDo.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheWonderfulScriptOfToDo : MonoBehaviour 
+{
+
+}
+/*
+KingZilla
+
+Everything marked with an "*" is for the prototype
+
+-Walk animation*
+-Punch animation*
+-Jump Animation*
+
+-Stomping animation
+-Kick animation
+
+Environment
+
+-Building destruction
+-2D art
+
+Script
+
+-basic gameplay (Moves side to side, jumps punch destroys building)
+-exploding 
+
+
+
+
+
+/*
